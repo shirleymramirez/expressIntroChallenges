44fa217585651aad9d5a85d14852dffaa58760fb
shirley <shirley.mramirez@yahoo.com>
2019-05-29 13:34:57 -0700

    done with part1

--- part1/server.js
+++ part1/server.js
@@ -1,15 +1,48 @@
 var express = require('express');
 var app = express();
+var bodyParse = require('body-parser');
+var fs = require('fs');
+app.use(bodyParse.json());
+
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+
+app.get('/hello', (req, res) => {
+  res.send("Hello");
 });
 
-app.use(function(req, res) {
+app.post('/create/:name', (req, res) => {
+  let obj = {
+    id: 1,
+    name: req.params.name
+  }
+  res.json(obj);
+})
+
+app.get('/', (req, res) => {
+  let index = fs.readFileSync("./part1/index.html", "utf8")
+  res.send(index)
+})
+
+
+app.get("/verify/:age", (req, res) => {
+  let age = parseInt(req.params.age);
+  if (age > 13) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(403);
+
+  }
+  res.send(req.body.good);
+});
+
+
+app.use(function (req, res) {
   res.sendStatus(404);
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log('Listening on port', port);
 });
+
+
